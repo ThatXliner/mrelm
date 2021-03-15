1f764cc628bb2b8ac380a980c94b90da76fb61f2
Bryan Hu <bryan.hu.2020@gmail.com>
2021-03-14 17:53:19 -0700

    ✨💄➕Added command-line options and added color to UI

--- mrelm/__main__.py
+++ mrelm/__main__.py
@@ -1,30 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Command line interface"""
+import argparse
 import os
-import sys
 
 import halo
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+from typing import Optional
 
 from . import git_utils, py_utils, utils
 
-REPO = os.getenv("REPO")
-
 
 def main() -> None:
-    if REPO is None:
-        raise ValueError("You must specify set `REPO` environment variable")
     lst_commit = git_utils.get_lst_commit()
-    print(
-        f"\N{RIGHT-POINTING MAGNIFYING GLASS} Last commit: {lst_commit.hash_id} |",
-        end=" ",
+    parser = argparse.ArgumentParser("mrelm")
+    parser.add_argument(
+        "repo",
+        nargs="?",
+        default=os.getenv("REPO_NAME") or os.getenv("REPO"),
+        help="The repository name",
     )
-    should_bootstrap = "--bootstrap" in sys.argv[1:]
-    if utils.is_version_bump(lst_commit.message) or should_bootstrap:
-        print(
-            " \N{WHITE HEAVY CHECK MARK} ",
-            ("Requested bootstrap" if should_bootstrap else "Version bump detected"),
+    parser.add_argument(
+        "--no-release", action="store_true", help="Do not upload to PyPI"
+    )
+    action_flags = parser.add_mutually_exclusive_group()
+    action_flags.add_argument(
+        "--bootstrap", "-b", action="store_true", help="Should bootstrap project?"
+    )
+    action_flags.add_argument(
+        "--generate-release-notes",
+        "-g",
+        action="store_true",
+        help="Generate release notes from the commits between the last 2 tags",
+    )
+    args = parser.parse_args()
+    repo: Optional[str] = args.repo
+    should_bootstrap: bool = args.bootstrap
+    should_generate_release_notes: bool = args.generate_release_notes
+    should_release: bool = not args.no_release
+    if repo is None:
+        utils.errorize(
+            "You must specify set `REPO_NAME` or `REPO` environment variable or"
+            " add the `--repo=Username/repo_name` argument"
         )
+    print(
+        f"\N{RIGHT-POINTING MAGNIFYING GLASS} Last commit:",
+        Fore.YELLOW + str(lst_commit.hash_id),
+    )
+    if utils.is_version_bump(lst_commit.message) or should_bootstrap:
+        if should_bootstrap:
+            print("\N{MANS SHOE} Requested bootstrap")
+        else:
+            print("\N{WHITE HEAVY CHECK MARK} Version bump detected")
         project_version = utils.get_project_version()
         print(f"\N{BOOKMARK} Project version detected: {project_version}")
         with halo.Halo("Building project...") as spinner:  # type: ignore
@@ -35,24 +64,26 @@ def main() -> None:
                 raise exception
             spinner.succeed()
         with halo.Halo("Creating and uploading release") as spinner:  # type: ignore
+            from_, to_ = git_utils.get_lst_tags(2)
             utils.create_release(
-                msg=git_utils.generate_release_notes(*git_utils.get_lst_tags(2)),
-                repo=REPO,
+                msg=git_utils.generate_release_notes(from_, to_),
+                repo=repo,
                 tag_name=f"v{project_version}",
                 title=f"Version v{project_version}",
                 artifacts=artifacts.artifacts,
                 commit_hash=lst_commit.hash_id,
             )
             spinner.succeed()
-        with halo.Halo("Publishing to PyPi") as spinner:  # type: ignore
-            py_utils.publish_for(py_utils.get_project_type())
-            spinner.succeed()
+        if should_release:
+            with halo.Halo("Publishing to PyPi") as spinner:  # type: ignore
+                py_utils.publish_for(py_utils.get_project_type())
+                spinner.succeed()
         artifacts.delete()
+    elif should_generate_release_notes:
+        from_, to_ = git_utils.get_lst_tags(2)
+        print(git_utils.generate_release_notes(from_, to_))
     else:
-        print("\N{CROSS MARK} Not a version bump")
-        print(
-            "\N{SLEEPING FACE} No work needs to be done: last commit was not version bump"
-        )
+        print("\N{SLEEPING FACE}", Fore.CYAN + "No work needs to be done")
 
 
 if __name__ == "__main__":
